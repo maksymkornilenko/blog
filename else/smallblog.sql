327f47601ec9e994ac8669e0e8e8abede475ae8f
maksymkornilenko <kornilenko1993@gmail.com>
2019-07-07 16:27:55 +0200

    add SQL smallblog

---
+++ else/smallblog.sql
@@ -0,0 +1,134 @@
+﻿-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Июл 07 2019 г., 17:27
+-- Версия сервера: 5.6.41
+-- Версия PHP: 7.0.32
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `smallblog`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `comments`
+--
+
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL,
+  `name` varchar(150) NOT NULL,
+  `text` text NOT NULL,
+  `dateOfCreate` timestamp NOT NULL,
+  `news_id` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `comments`
+--
+
+INSERT INTO `comments` (`id`, `name`, `text`, `dateOfCreate`, `news_id`) VALUES
+(74, '123124', '325354353', '2019-07-07 14:07:03', 30),
+(75, '54365464564', 'tt546756756754754634643', '2019-07-07 14:07:02', 32),
+(76, '21412323`131`32``', '32143424325', '2019-07-07 14:07:39', 30),
+(77, '214235435', '34213243253452', '2019-07-07 14:07:44', 30),
+(78, '654654645645456456', '432543534645', '2019-07-07 14:07:51', 30),
+(79, '5465464565464564645`', '64565675676', '2019-07-07 14:07:08', 30),
+(80, '11111112222222', '2222211111111', '2019-07-07 14:07:29', 32),
+(81, '33333333333', '1111111111111111', '2019-07-07 14:07:34', 32),
+(82, '11111111111', '44444444444', '2019-07-07 14:07:40', 32),
+(83, 'qwe', 'ewq', '2019-07-07 14:07:49', 32),
+(84, 'q', 'q', '2019-07-07 14:07:00', 32),
+(85, '123123', '23123', '2019-07-07 14:07:09', 31),
+(86, '243423424', '321324324', '2019-07-07 14:07:13', 31),
+(87, '324234', '2131234', '2019-07-07 14:07:21', 33),
+(88, '2', '2', '2019-07-07 14:07:31', 35),
+(89, '453543543', '12312434', '2019-07-07 14:07:51', 34);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `news`
+--
+
+CREATE TABLE `news` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(150) DEFAULT NULL,
+  `text` text,
+  `dateOfCreate` timestamp NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `news`
+--
+
+INSERT INTO `news` (`id`, `name`, `text`, `dateOfCreate`) VALUES
+(30, '124324235', '4365465746364353', '2019-07-07 14:07:34'),
+(31, '213124', '43235436', '2019-07-07 14:07:53'),
+(32, 'gfdgfdhgfhfghgfh', 'Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsum, quia dolor sit, amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt, ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit, qui in ea voluptate velit esse, quam nihil molestiae consequatur, vel illum, qui dolorem eum fugiat, quo voluptas nulla pariatur? At vero eos et accusamus et iusto odio dignissimos ducimus, qui blanditiis praesentium voluptatum deleniti atque corrupti, quos dolores et quas molestias excepturi sint, obcaecati cupiditate non provident, similique sunt in culpa, qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio, cumque nihil impedit, quo minus id, quod maxime placeat, facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet, ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.', '2019-07-07 14:07:22'),
+(33, '1321412', '25431546486876', '2019-07-07 14:07:10'),
+(34, '65465756756756745754', '6547697896464354354', '2019-07-07 14:07:15'),
+(35, '1', '1', '2019-07-07 14:07:14'),
+(36, '1', '321312', '2019-07-07 14:07:18'),
+(37, '31243423', '124rrwerfwe', '2019-07-07 14:07:01');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `news_id` (`news_id`);
+
+--
+-- Индексы таблицы `news`
+--
+ALTER TABLE `news`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=90;
+
+--
+-- AUTO_INCREMENT для таблицы `news`
+--
+ALTER TABLE `news`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`news_id`) REFERENCES `news` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
